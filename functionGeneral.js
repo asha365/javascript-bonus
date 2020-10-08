921ff339c33ae5794d3777eb64dbce8f0aa1fbd6
Fatima (Asha Afrin) <asha.engrbbd@gmail.com>
2020-10-08 14:24:09 +0600

    added bonus modiul functionGeneral file

---
+++ functionGeneral.js
@@ -0,0 +1,14 @@
+ //local variable
+ function addTodo(task){
+     const newTaskElement = document.createElement('li');
+}
+console.log(newTaskElement);
+
+//global variable
+var name = 'asha';
+
+function addUser(){
+    console.log(name);
+}
+addUser();
+
